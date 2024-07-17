19bf8a8a750852d0d7d6b8bf8150e3fd0b6026e8
Sourov <sourovdas730@gmail.com>
2024-07-17 17:45:28 +0600

    complete log in and reg

--- src/page/login/Login.jsx
+++ src/page/login/Login.jsx
@@ -9,13 +9,26 @@ import useAxiosSecure from '../../hook/useAxiosSecure';
 const Login = () => {
     const [open, setOpen] = useState(false);
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const [loading, setLoading] = useState(false);
+    const [logError, setLogError] =  useState(null);
+    const [logSuccess, setLogSuccess] =  useState(null);
     const axiosSecure = useAxiosSecure();
 
     const onSubmit = async (data) => {
+        setLoading(true);
         console.log(data)
 
         const res = await axiosSecure.post(`/log-user`, data)
         console.log(res.data);
+        if(res.data?.message){
+            setLogError(res.data?.message);
+            setLoading(false);
+        }
+        else{
+            setLogSuccess('success');
+            setLoading(false);
+            setLogError(null);
+        }
 
     };
     return (
@@ -23,16 +36,24 @@ const Login = () => {
 
             <form onSubmit={handleSubmit(onSubmit)} className='w-full lg:w-1/3 flex flex-col justify-center items-center gap-4 text-[#006769]'>
                 <img className='mb-9' src={logo} alt="" />
+                <p className='text-white'>{logError}</p>
                 <div className='w-full'>
-                <input {...register('userIdentity', {required : true})} className='py-3 px-4 w-full outline-none focus:outline-gray-100' type="text" placeholder='Phone or Email' />
-                {errors.userIdentity && <span className='text-white text-start inline-block w-full'>Phone or Email is required</span>}
+                    <input {...register('userIdentity', { required: true })} className='py-3 px-4 w-full outline-none focus:outline-gray-100' type="text" placeholder='Phone or Email' />
+                    {errors.userIdentity && <span className='text-white text-start inline-block w-full'>Phone or Email is required</span>}
                 </div>
                 <div className='w-full relative'>
-                    <input {...register('password', {required : true})} className='py-3 px-4 w-full outline-none focus:outline-gray-100' type={!open ? 'password' : 'text'} placeholder='Password' />
+                    <input {...register('password', { required: true })} className='py-3 px-4 w-full outline-none focus:outline-gray-100' type={!open ? 'password' : 'text'} placeholder='Password' />
                     <span onClick={() => setOpen(!open)} className='absolute pt-4 right-0 px-2'>{!open ? <FaRegEyeSlash /> : <IoEyeOutline />}</span>
                     {errors.password && <span className='text-white text-start inline-block w-full'>Password is required</span>}
                 </div>
-                <input className='py-3 w-1/3 border border-white text-white hover:bg-white hover:text-[#006769]' type="submit" value='Log In' />
+                {
+                    loading ?
+                        <button className='py-3 w-1/3 bg-white text-[#006769]  flex justify-center items-center'>
+                            <span className="loading loading-spinner loading-sm"></span>
+                        </button> :
+                        <input className='py-3 w-1/3 border border-white text-white hover:bg-white hover:text-[#006769]' type="submit" value='Sign Up' />
+                }
+                {/* <input className='py-3 w-1/3 border border-white text-white hover:bg-white hover:text-[#006769]' type="submit" value='Log In' /> */}
                 <div className='mt-4'>
                     <Link to={'/sign-up'} className='text-white border-b hover:border-b-2'>Create Account</Link>
                 </div>
--- src/page/signUp/SignUp.jsx
+++ src/page/signUp/SignUp.jsx
@@ -10,12 +10,26 @@ const SignUp = () => {
     const [open, setOpen] = useState(false);
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const axiosSecure = useAxiosSecure();
-
+    const [regError, setRegError] = useState(null);
+    const [regSuccess, setRegSuccess] = useState(null);
+    const [loading, setLoading] = useState(false);
+    console.log(errors)
     const onSubmit = async (data) => {
+        setRegError(null);
+        setRegSuccess(null);
+        setLoading(true);
         console.log(data)
 
         const res = await axiosSecure.post(`/reg-user`, data)
         console.log(res.data);
+        if (res.data?.message) {
+            setRegError(res.data?.message);
+            setLoading(false);
+        }
+        else {
+            setRegSuccess('success');
+            setLoading(false);
+        }
 
     };
 
@@ -24,7 +38,9 @@ const SignUp = () => {
 
             <form onSubmit={handleSubmit(onSubmit)} className='w-full lg:w-1/3 flex flex-col justify-center items-center gap-4 text-[#006769]'>
                 <img className='mb-9' src={logo} alt="" />
+                <p className='text-white'>{regError}</p>
                 <div className='w-full'>
+
                     <input {...register('name', { required: true })} className='py-3 px-4 w-full outline-none focus:outline-gray-100' type="text" placeholder='Name' />
                     {errors.name && <span className='text-white text-start inline-block w-full'>Name is required</span>}
                 </div>
@@ -37,23 +53,30 @@ const SignUp = () => {
                     {errors.email && <span className='text-white text-start inline-block w-full'>Email is required</span>}
                 </div>
                 <div className='w-full relative'>
-                    <input {...register('password', { required: true })} className='py-3 px-4 w-full outline-none focus:outline-gray-100' type={!open ? 'password' : 'text'} placeholder='Password' />
+                    <input {...register('password', { required: true, pattern: /^\d{5}$/ })} className='py-3 px-4 w-full outline-none focus:outline-gray-100' type={!open ? 'password' : 'text'} placeholder='Password' />
                     <span onClick={() => setOpen(!open)} className='absolute pt-4 right-0 px-2'>{!open ? <FaRegEyeSlash /> : <IoEyeOutline />}</span>
-                    {errors.password && <span className='text-white text-start inline-block w-full'>Password is required</span>}
+                    {errors.password?.type === 'required' && <span className='text-white text-start inline-block w-full'>Password is required</span>}
+                    {errors.password?.type === 'pattern' && <span className='text-white text-start inline-block w-full'>Password must be 5 digit number</span>}
                 </div>
                 <div className='text-white flex justify-center items-center gap-9'>
                     <label className='inline-flex items-center gap-2'>
-                        <input {...register('role')} required className='accent-[#006769] w-4 h-4' type="radio" value={'user'}/>
+                        <input {...register('role')} required className='accent-[#006769] w-4 h-4' type="radio" value={'user'} />
                         <span>User</span>
                     </label>
 
                     <label className='inline-flex items-center gap-2'>
-                        <input {...register('role')} required className='accent-[#006769] w-4 h-4' type="radio" value={'agent'}/>
+                        <input {...register('role')} required className='accent-[#006769] w-4 h-4' type="radio" value={'agent'} />
                         <span>Agent</span>
                     </label>
 
                 </div>
-                <input className='py-3 w-1/3 border border-white text-white hover:bg-white hover:text-[#006769]' type="submit" value='Sign Up' />
+                {
+                    loading ?
+                        <button className='py-3 w-1/3 bg-white text-[#006769]  flex justify-center items-center'>
+                            <span className="loading loading-spinner loading-sm"></span>
+                        </button> :
+                        <input className='py-3 w-1/3 border border-white text-white hover:bg-white hover:text-[#006769]' type="submit" value='Sign Up' />
+                }
                 <div className='mt-4'>
                     <Link to={'/'} className='text-white border-b hover:border-b-2'>Log In</Link>
                 </div>
