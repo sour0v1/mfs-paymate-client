f639682ec1964d132a5a504f1671f183340fa605
Sourov <sourovdas730@gmail.com>
2024-07-28 21:05:19 +0600

    responsive transaction history

--- src/dashboard/users/userTransactions/UserTransactions.jsx
+++ src/dashboard/users/userTransactions/UserTransactions.jsx
@@ -10,7 +10,7 @@ import './UserTransaction.css'
 const UserTransactions = () => {
 
     return (
-        <div className='bg-[#006769] text-white pt-16 pb-6 min-h-screen relative'>
+        <div className='bg-[#006769] text-white pt-16 pb-6 min-h-screen relative px-4 lg:px-0'>
             <div className='absolute top-4 left-4 flex justify-center items-center gap-4'>
                 <Link to={'/home'} className='text-white text-2xl'><FaHome /></Link>
                 <div className='pl-2 border-l-2 flex justify-center items-center  gap-1 py-3'>
@@ -18,12 +18,12 @@ const UserTransactions = () => {
                     <span className='text-white'>Transactions History</span>
                 </div>
             </div>
-            <div className='max-w-6xl mx-auto flex justify-center items-start gap-6'>
-                <div id='transaction' className='px-6 border min-h-screen flex flex-col items-start gap-3 py-3 w-fit'>
+            <div className='max-w-6xl mx-auto flex flex-col lg:flex-row justify-center items-start gap-6 my-3'>
+                <div id='transaction' className='px-6 border h-full lg:min-h-screen flex flex-col items-start gap-3 py-3 w-full lg:w-fit'>
                     <NavLink to={'history/send-money'}>Send Money</NavLink>
                     <NavLink to={'history/recieve-money'}>Recieve Money</NavLink>
                 </div>
-                <div className='w-3/4'>
+                <div className='w-full lg:w-3/4'>
                     <Outlet></Outlet>
                 </div>
 
